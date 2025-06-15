966b80f79a09191d4cd27e096f1678ac2f2ec156
Hrach Sahakyan <62832144+aacc1on@users.noreply.github.com>
2025-06-15 13:51:03 +0400

    Update users.js

--- SimpleStore/routes/users.js
+++ SimpleStore/routes/users.js
@@ -23,7 +23,7 @@ router.get('/:id', authenticateJWT, authhorizeRole('admin'), async (req, res) =>
     try {
         const { id }  = req.params;
         const result = await pool.query('SELECT FROM users WHERE if = $1', [id]);
-        if (result === 0) {
+        if (result.length === 0) {
             return res.status(404).json({ message: 'User not found' });
         }
         res.status(200).json(result.rows[0]);
